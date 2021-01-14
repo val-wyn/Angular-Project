71f513f252733b6f55314f58e03820c1936bbde5
valentin.wynands <valentin.wynands@epsi.fr>
2021-01-14 12:03:21 +0100

    main

--- src/app/core/can-activate.guard.ts
+++ src/app/core/can-activate.guard.ts
@@ -4,6 +4,7 @@ import {Observable, of} from 'rxjs';
 import {AuthService} from './services/auth.service';
 import {catchError, map} from 'rxjs/operators';
 import {User} from './entities/user';
+import {MatSnackBar} from '@angular/material';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,8 @@ export class CanActivateGuard implements CanActivate {
 
   constructor(
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private snackBar: MatSnackBar
   ) {
   }
 
@@ -35,12 +37,18 @@ export class CanActivateGuard implements CanActivate {
         // on retourne true ou false en fonction du status ( et donc de la permission )
         if ('status' in response) {
           if (401 === response.status || 403 === response.status) {
+            this.snackBar.open(' Vous devez être connecté pour accéder à cette page', 'Fermer', {
+              duration: 2000,
+            });
             this.router.navigate(['auth/signin']);
             return false;
           }
           // on vérifie que l'utilisateur est admin dans le cas où la router est dédié à des admins
         } else if ( 'roles' in response ) {
           if (!response.roles.includes('ROLE_ADMIN') && ('admin' in next.data) ) {
+            this.snackBar.open(' Seul un administrateur peut avoir accès au profil', 'Fermer', {
+              duration: 2000,
+            });
             this.router.navigate(['auth/signin']);
             return false;
           }
@@ -49,6 +57,8 @@ export class CanActivateGuard implements CanActivate {
       })
     );
   }
+
+
 
 }
 
